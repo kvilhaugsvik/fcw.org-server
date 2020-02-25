de49ce62920078fa51e70d77bdb32dc758735789
Sveinung Kvilhaugsvik <sveinung84@users.sourceforge.net>
2020-02-25 14:15:11 +0100

    Action probability for "Poison City".
    Calculate action probability for "Poison City" and for "Poison City Escape".
    Will almost always be unknown because of potential invisible diplomatic
    defenders.
    
    See hrm Feature #861827
    
    (cherry picked from commit ae422ce44627ead01a5a8ce350427b526147c8c2)

--- freeciv/freeciv/common/actions.c
+++ freeciv/freeciv/common/actions.c
@@ -4108,8 +4108,6 @@ action_prob(const action_id wanted_action,
 
   switch (wanted_action) {
   case ACTION_SPY_POISON:
-    /* TODO */
-    break;
   case ACTION_SPY_POISON_ESC:
     /* All uncertainty comes from potential diplomatic battles. */
     chance = ap_diplomat_battle(actor_unit, NULL, target_tile);
